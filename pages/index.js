0e7d3317d3011676772a291390095a6d35633881
ra72e6 <ra72e6@hanmail.net>
2022-12-02 15:28:10 +0900

    3th

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
-import Head from 'next/head';
-import Image from 'next/image';
+//import Head from 'next/head';
+//import Image from 'next/image';
 import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css';
+import ProductItem from '../components/ProductItem';
+//import styles from '../styles/Home.module.css';
+import data from '../utils/data';
 
 export default function Home() {
-  return <Layout title="Home">Homepage</Layout>;
+  return (
+    <Layout title="Home">
+      <div className="grid grid-cols-1 gap-4 mid:gird-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
